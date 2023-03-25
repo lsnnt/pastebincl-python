e422fedf5455d653a885d8841e60a1a42e679b79
nityanand86876 <85880458+nityanand86876@users.noreply.github.com>
2023-03-25 15:30:51 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+filename = "pi.txt" # replace with the path of your file
+
+with open(filename, "rb") as file:
+
+	data = {
+	    'api_dev_key': '<--enter your apidev key here-->', //enter your apidev key here
+	    'api_paste_code': file,
+	    'api_option': 'paste',
+	}
+
+	response = requests.post('https://pastebin.com/api/api_post.php', data=data)
+
+	print(response.text)
