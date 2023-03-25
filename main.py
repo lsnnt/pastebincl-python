4764b09cdab442a15d413ab7090d0e70fc1dc5bc
nnt <you@example.com>
2023-03-25 12:10:45 +0000

    improved code

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import requests
-filename=input("Input file location ==> ")
-#filename = "pi.txt" # replace with the path of your file
 
-with open(filename, "rb") as file:
+# Replace with your developer API key
+api_key = 'LQR2wg5QWv4ZTI7_smidulNQCB-2MvAf'
 
-	data = {
-	    'api_dev_key': '<--enter your apidev key here-->', # enter your apidev key here
-	    'api_paste_code': file,
-	    'api_option': 'paste',
-	}
+# Specify the file path
+file_path = '/home/test/flags.json'
 
-	response = requests.post('https://pastebin.com/api/api_post.php', data=data)
+# Read the content of the file
+with open(file_path, 'r') as f:
+    file_content = f.read()
 
-	print(response.text)
+# Set the parameters for the Pastebin API request
+api_url = 'https://pastebin.com/api/api_post.php'
+data = {
+    'api_dev_key': api_key,
+    'api_option': 'paste',
+    'api_paste_code': file_content
+}
+
+# Make the API request
+response = requests.post(api_url, data=data)
+
+# Print the Pastebin URL if successful
+if response.ok:
+    print('File uploaded to Pastebin:')
+    print(response.text)
+else:
+    print('Error uploading file to Pastebin')
+
