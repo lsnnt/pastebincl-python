9ffe79ce15242d6e0aaaa5bdaf41f9e733766601
nityanand86876 <85880458+nityanand86876@users.noreply.github.com>
2023-03-25 15:34:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import requests
-filename = "pi.txt" # replace with the path of your file
+filename=input("Input file location ==> ")
+#filename = "pi.txt" # replace with the path of your file
 
 with open(filename, "rb") as file:
 
 	data = {
-	    'api_dev_key': '<--enter your apidev key here-->', //enter your apidev key here
+	    'api_dev_key': '<--enter your apidev key here-->', # enter your apidev key here
 	    'api_paste_code': file,
 	    'api_option': 'paste',
 	}
